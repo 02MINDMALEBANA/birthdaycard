2d7fe2833f3d94d441ac82058476664e30b76469
02MINDMALEBANA <malebanakgopo@gmail.com>
2022-09-16 13:12:22 +0200

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, Image } from 'react-native';
+import pic from './assets/R.gif'
+// import pic1 from './assets/gifb.gif'
+import pic1 from './assets/bd1.png'
+import bd from './assets/anima.gif'
 
+//width:"100%", height:700, 
+// const pic = { uri: "https://reactjs.org/logo-og.png" };
 export default function App() {
+ 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground source={pic1} resizeMode="cover" style={{justifyContent:"center",flex:1}}>
+        <View><Image source={bd} style={{width:300, height:100, marginLeft:30}}></Image></View>
+      {/* <Text style={styles.happy}>Happy                 Birthday</Text> */}
+        <View style={styles.card}> 
+        
+        <Text style={styles.hpyb}>wishing you a magical birthday filled with splendid surprise</Text>
+        <Image source={pic} style={{width:170, height:200, marginLeft:30}}></Image>
+
+        </View>
+        {/* <Text>let</Text> */}
+
+      </ImageBackground>
+  
     </View>
   );
 }
@@ -13,8 +31,51 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
+  image: {
+    flex: 1,
+    justifyContent: "center",
+    width: 100
+    
+  },
+  card:{
+    width:200,
+    height: 400,
+    // borderWidth:2,
+    marginLeft:170,
+    // backgroundColor:'pink',
+    opacity:0.7,
+    borderRadius:10,
+    marginTop:100,
+    
+  },
+  happy:{
+    fontFamily:"Lucida Handwriting,Lucida,Cursive",
+    fontSize:20,
+    fontWeight:'bold',
+    color:"red",
+    marginLeft:40,
+    marginBottom:0,
+    
+
+
+  },
+  hpyb:{
+    // fontFamily:"Lucida Handwriting,Lucida,Cursive",
+    fontSize:17,
+    fontWeight:'bold',
+    fontFamily:"Times New Roman, Times, serif",
+    // color:'#00008b',
+    color:'red',
+    marginTop:100,
+    marginLeft:33,
+    textTransform:'uppercase',
+    
+    
+
+
+  }
 });
